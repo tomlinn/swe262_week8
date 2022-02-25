ae46ca1b729440c0fcaf950528017007cc3c4f24
tomlin <tomlin@dhcp-v043-086.mobile.uci.edu>
2022-02-24 19:00:09 -0800

    add - implement all from python file but not test it yet

--- Thirty.java
+++ Thirty.java
@@ -1,57 +1,74 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import static java.util.Collections.reverseOrder;
+import static java.util.Map.Entry.comparingByValue;
+
 public class Thirty {
+    static BlockingQueue<String> word_space = new LinkedBlockingQueue<>();
+    static List<String> stopwords;
+    static BlockingQueue<Map<String,Integer>> freq_space = new LinkedBlockingQueue<>();
+    static List<String> words;
     public static void main(String[] args) throws Exception {
-        List<String> stopwords = Arrays.asList(Files.readString(Paths.get("stop_words.txt")).split(","));
-        BlockingQueue<String> word_space = new LinkedBlockingQueue<>();
-        BlockingQueue<Map<String,Integer>> freq_space = new LinkedBlockingQueue<>();
-        List<String> words = Arrays.asList(Files.readString(Paths.get("pride-and-prejudice.txt")).toLowerCase().split("[^a-z]+"));
+        stopwords = Arrays.asList(Files.readString(Paths.get("stop_words.txt")).split(","));
+        words = Arrays.asList(Files.readString(Paths.get("pride-and-prejudice.txt")).toLowerCase().split("[^a-z]+"));
         for(String word : words){
             word_space.put(word);
         }
-    }
-    // # Let's create the workers and launch them at their jobs
-    // workers = []
-    // for i in range(5):
-    //     workers.append(threading.Thread(target = process_words))
-    // [t.start() for t in workers]
-    //
-    // # Let's wait for the workers to finish
-    // [t.join() for t in workers]
-    //
-    // # Let's merge the partial frequency results by consuming
-    // # frequency data from the frequency space
-    // word_freqs = {}
-    // while not freq_space.empty():
-    //     freqs = freq_space.get()
-    //     for (k, v) in freqs.items():
-    //         if k in word_freqs:
-    //             count = sum(item[k] for item in [freqs, word_freqs])
-    //         else:
-    //             count = freqs[k]
-    //         word_freqs[k] = count
-    //
-    // for (w, c) in sorted(word_freqs.items(), key=operator.itemgetter(1), reverse=True)[:25]:
-    //     print(w, '-', c)
 
-    public void process_words(){
-        // word_freqs = {}
-        //    while True:
-        //        try:
-        //            word = word_space.get(timeout=1)
-        //        except queue.Empty:
-        //            break
-        //        if not word in stopwords:
-        //            if word in word_freqs:
-        //                word_freqs[word] += 1
-        //            else:
-        //                word_freqs[word] = 1
-        //    freq_space.put(word_freqs)
+        List<Thread> workers = null;
+        for (int i = 0; i < 5; i++) {
+            Thread t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    process_words();
+                }
+            });
+            workers.add(t1);
+        }
+        for (int i = 0; i < 5; i++) {
+            workers.get(i).join();
+        }
+
+
+
+        Map<String, Integer> word_freqs = new HashMap<>();
+        while (!freq_space.isEmpty()){
+            Map<String, Integer> freqs = freq_space.take();
+            word_freqs.putAll(freqs);
+        }
+        word_freqs.entrySet().stream().sorted(comparingByValue(reverseOrder()))
+                .limit(25)
+                .forEach(word -> System.out.println(word.getKey() + " - " + word.getValue()));
+    }
+
+
+    public static void process_words(){
+        Map<String ,Integer> word_freqs = new HashMap<>();
+        while(true) {
+            String word;
+            try {
+                word = word_space.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+            if (!stopwords.contains(word)){
+                if(word_freqs.keySet().contains(word)){
+                    word_freqs.put(word,word_freqs.get(word)+1);
+                }
+                else{
+                    word_freqs.put(word,1);
+                }
+            }
+            try {
+                freq_space.put(word_freqs);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
