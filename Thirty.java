33f756a01f3ad7e7cb246bf7892f53b45707ff23
Ching-Yang Lin <chingyal@uci.edu>
2022-02-25 21:52:44 -0800

    Done - Thirty

--- Thirty.java
+++ Thirty.java
@@ -3,6 +3,7 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Collections.reverseOrder;
 import static java.util.Map.Entry.comparingByValue;
@@ -16,10 +17,12 @@ public class Thirty {
         stopwords = Arrays.asList(Files.readString(Paths.get("stop_words.txt")).split(","));
         words = Arrays.asList(Files.readString(Paths.get("pride-and-prejudice.txt")).toLowerCase().split("[^a-z]+"));
         for(String word : words){
-            word_space.put(word);
+            if(word.length() >= 2) {
+                word_space.put(word);
+            }
         }
 
-        List<Thread> workers = null;
+        List<Thread> workers = new ArrayList<Thread>();
         for (int i = 0; i < 5; i++) {
             Thread t1 = new Thread(new Runnable() {
                 @Override
@@ -28,6 +31,10 @@ public class Thirty {
                 }
             });
             workers.add(t1);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            workers.get(i).start();
         }
         for (int i = 0; i < 5; i++) {
             workers.get(i).join();
@@ -38,7 +45,13 @@ public class Thirty {
         Map<String, Integer> word_freqs = new HashMap<>();
         while (!freq_space.isEmpty()){
             Map<String, Integer> freqs = freq_space.take();
-            word_freqs.putAll(freqs);
+            for(String key: freqs.keySet()){
+                if(!word_freqs.keySet().contains(key)){
+                    word_freqs.put(key,freqs.get(key));
+                }else{
+                    word_freqs.put(key,word_freqs.get(key) + freqs.get(key));
+                }
+            }
         }
         word_freqs.entrySet().stream().sorted(comparingByValue(reverseOrder()))
                 .limit(25)
@@ -49,12 +62,14 @@ public class Thirty {
     public static void process_words(){
         Map<String ,Integer> word_freqs = new HashMap<>();
         while(true) {
-            String word;
+            String word = null;
             try {
-                word = word_space.take();
+                word = word_space.poll(1, TimeUnit.SECONDS);
+                if(word == null){
+                    break;
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                break;
             }
             if (!stopwords.contains(word)){
                 if(word_freqs.keySet().contains(word)){
@@ -64,11 +79,11 @@ public class Thirty {
                     word_freqs.put(word,1);
                 }
             }
-            try {
-                freq_space.put(word_freqs);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        }
+        try {
+            freq_space.put(word_freqs);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
